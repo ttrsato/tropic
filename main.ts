149c95c573923652c2bee874e547b7ce41152089
ttrsato <tatsurosatoh@gmail.com>
2019-05-02 22:22:48 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,26 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Types of tropical fruit
+ */
+enum TropicalFruit {
+    Banana = 0,
+    Pinapple = 1,
+    Coconut = 2
+}
+
+/**
+ * Pick some fruit and peel it.
+ */
+namespace tropic {
+    /**
+     * Pick a fruit
+     */
+    export function pick(fruit: TropicalFruit): boolean {
+        return true;
+    }
+    /**
+     * Peel the fruit if possible
+     */
+    export function peel(fruit: TropicalFruit): boolean {
+        return (fruit == TropicalFruit.Banana);
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
+let peeled = false;
+if (tropic.pick(TropicalFruit.Banana)) {
+    peeled = tropic.peel(TropicalFruit.Banana);
+}
