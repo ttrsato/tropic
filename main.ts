4b0a10e140e95caa46b83fb8e3381d0dd4134b58
ttrsato <tatsurosatoh@gmail.com>
2019-05-03 03:15:12 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=70 icon="\uf075" color=#555555 block="コメント"
 namespace comment {
     //% blockId=show_strings block="コメント %v"
-    export function noaction(text: string): void {
+    export function init(tx: SerialPin): void {
     }
 }
